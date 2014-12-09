4172a8cd27ea34e0a5fa5561f59b8ad41f318425
Mario Young <maye.co@gmail.com>
2014-12-08 23:58:53 -0500

    Update MayecoNotificationsExtension.php

--- DependencyInjection/MayecoNotificationsExtension.php
+++ DependencyInjection/MayecoNotificationsExtension.php
@@ -18,9 +18,7 @@
 namespace Mayeco\NotificationsBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 class MayecoNotificationsExtension extends Extension
 {
